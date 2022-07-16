e5f0a708109b8eee4254147b9f2f42346cc8580e
alaashaban22 <95075380+alaashaban22@users.noreply.github.com>
2022-07-16 15:25:32 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,238 @@
+let pt1 = document.getElementById('pt1');
+let pt2 = document.getElementById('pt2');
+let pt3 = document.getElementById('pt3');
+let pt4 = document.getElementById('pt4');
+let pt5 = document.getElementById('pt5');
+let pt6 = document.getElementById('pt6');
+let pt7 = document.getElementById('pt7');
+let pt8 = document.getElementById('pt8');
+let pt9 = document.getElementById('pt9');
+let pt10 = document.getElementById('pt10');
+let pt11 = document.getElementById('pt11');
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
+let p5 = document.getElementById('p5');
+let p6 = document.getElementById('p6') ;
+let p7 = document.getElementById('p7') ;
+let p8 = document.getElementById('p8') ;
+let p9 = document.getElementById('p9') ;
+let p10 = document.getElementById('p10') ;
+let p11 = document.getElementById('p11') ;
+/* ====================================== */
+
+function pm1(){
+ 
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+}
+pt1.onclick = pm1 ;
+
+
+
+function pm2(){
+ 
+    p2.style.transform = 'translateX(-222%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+    
+}
+pt2.onclick = pm2 ;
+
+
+function pm3(){
+ 
+    p3.style.transform = 'translateX(-222%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+
+}
+pt3.onclick = pm3 ;
+
+
+function pm4(){
+ 
+    p4.style.transform = 'translateX(-222%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+
+}
+pt4.onclick = pm4 ;
+function pm5(){
+ 
+    p5.style.transform = 'translateX(-222%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+
+}
+pt5.onclick = pm5 ;
+
+function pm6(){
+    p6.style.transform = 'translateX(-222%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p7.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+}
+pt6.onclick = pm6 ;
+
+
+
+function pm7(){
+    p7.style.transform = 'translateX(-222%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)' ;
+    p9.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+
+}
+pt7.onclick = pm7 ;
+
+function pm8(){
+    p9.style.transform = 'translateX(200%)';
+    p7.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(-222%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+
+}
+pt8.onclick = pm8 ;
+
+function pm9(){
+    p9.style.transform = 'translateX(-222%)';
+    p7.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)';
+    p10.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+}
+pt9.onclick = pm9 ;
+
+function pm10(){
+    p10.style.transform = 'translateX(-222%)';
+    p9.style.transform = 'translateX(200%)';
+    p7.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(200%)';
+}
+pt10.onclick = pm10 ;
+
+
+function pm11(){
+    p10.style.transform = 'translateX(200%)';
+    p9.style.transform = 'translateX(200%)';
+    p7.style.transform = 'translateX(200%)' ;
+    p5.style.transform = 'translateX(200%)' ;
+    p2.style.transform = 'translateX(200%)' ;
+    p3.style.transform = 'translateX(200%)' ;
+    p4.style.transform = 'translateX(200%)' ;
+    p6.style.transform = 'translateX(200%)' ;
+    p8.style.transform = 'translateX(200%)';
+    p11.style.transform = 'translateX(-222%)';
+}
+pt11.onclick = pm11 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var imgList = Array.from(document.querySelectorAll(' img') );
+// var itemContainerBox = document.getElementById('itemContainerBox') ;
+// var itemBoxGall      = document.getElementById('itemBoxGall');
+
+// for( var i = 0 ; i < imgList.length ; i++)
+// {
+//     imgList[i].addEventListener('click' ,function(e){
+//         itemContainerBox.style.display = "flex" ;
+//         console.log("hellllo");
+//     })
+// }
+
+
+
+
+
+
+
+
+
